01e32e99ed572d43da8e4734bfafc15c9f714df2
alexjames0419 <alexanderjamesacojido@gmail.com>
2024-11-13 09:40:39 +0800

    Add files via upload

---
+++ Laboratoryexercise6.java
@@ -0,0 +1,38 @@
+
+package laboratoryexercise6;
+
+
+public class Laboratoryexercise6 {
+
+  
+    public static void main(String[] args) {
+        int iNumber = 3;
+        if (iNumber >=0)
+        {
+        
+             System.out.println("first string");
+         
+             if (iNumber == 0){ 
+             {
+             System.out.println("second string");
+             System.out.println("third string")   ;
+             
+             
+             
+         }
+            
+        }
+        
+
+        
+        
+      
+            
+        
+        
+   
+          
+      }
+    }
+    
+
